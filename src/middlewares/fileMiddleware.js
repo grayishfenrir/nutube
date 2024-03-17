0bfb6260e33f406c03e0ae80a25c1782c5858e85
Dean <dean.kang@mvlchain.io>
2024-03-17 16:32:57 +0900

    feat: split s3 folder for each purpose

--- src/middlewares/fileMiddleware.js
+++ src/middlewares/fileMiddleware.js
@@ -2,18 +2,23 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 
-
-
 const s3 = new S3Client({
     region: "ap-northeast-2",
 });
 
-const multerS3Storage = multerS3({
+export const isProd = process.env.NODE_ENV === "prod";
+
+const s3ProfileStorage = multerS3({
     s3: s3,
-    bucket: "nutube-files",
+    bucket: "nutube-files/profiles",
     acl: "public-read",
 });
 
+const s3VideoStorage = multerS3({
+    s3: s3,
+    bucket: "nutube-files/videos",
+    acl: "public-read",
+});
 
-export const uploadProfile = multer({ dest: 'uploads/userProfiles', limits: { fileSize: 3000000 }, storage: multerS3Storage });
-export const uploadVideo = multer({ dest: 'uploads/videos', limits: { fileSize: 15000000 }, storage: multerS3Storage });
+export const uploadProfile = multer({ dest: 'uploads/userProfiles', limits: { fileSize: 3000000 }, storage: s3ProfileStorage });
+export const uploadVideo = multer({ dest: 'uploads/videos', limits: { fileSize: 15000000 }, storage: s3VideoStorage });
--- src/middlewares/localsMiddleware.js
+++ src/middlewares/localsMiddleware.js
@@ -1,7 +1,9 @@
+import { isProd } from "./fileMiddleware.js";
+
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteTitle = "nutube";
-
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.loggedInUser || {};
+    res.locals.isProd = isProd;
     next();
 };
