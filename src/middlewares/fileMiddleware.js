2b11c8b0fb7eebd9ec8fc5d10eceaf58434e8402
Dean <dean.kang@mvlchain.io>
2024-03-17 16:40:01 +0900

    fix: del / in bucket name

--- src/middlewares/fileMiddleware.js
+++ src/middlewares/fileMiddleware.js
@@ -12,12 +12,24 @@ const s3ProfileStorage = multerS3({
     s3: s3,
     bucket: "nutube-files/profiles",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (request, file, ab_callback) {
+        const newFileName = Date.now() + "-" + file.originalname;
+        const fullPath = "profiles/" + newFileName;
+        ab_callback(null, fullPath);
+    },
 });
 
 const s3VideoStorage = multerS3({
     s3: s3,
     bucket: "nutube-files/videos",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (request, file, ab_callback) {
+        const newFileName = Date.now() + "-" + file.originalname;
+        const fullPath = "videos/" + newFileName;
+        ab_callback(null, fullPath);
+    },
 });
 
 export const uploadProfile = multer({ dest: 'uploads/userProfiles', limits: { fileSize: 3000000 }, storage: s3ProfileStorage });
